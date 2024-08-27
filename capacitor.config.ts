7bbcf7d0ed33f3bdd752542ac16a83beb09f6f11
Bananadev3 <angelomodanez30@gmail.com>
2024-08-27 12:04:20 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'songList',
+  webDir: 'www'
+};
+
+export default config;
